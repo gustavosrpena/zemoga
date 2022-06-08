bbef1f556de9a7163f0b333c9c01ab869f9767cf
Caio_lps <caio.lps2@gmail.com>
2022-06-08 11:53:51 -0300

    working on the voting system

--- client/src/components/cards-section/card/Card.js
+++ client/src/components/cards-section/card/Card.js
@@ -4,14 +4,14 @@ import { useState } from "react"
 
 export const Card = () => {
 
-    const props = {
-        vote: {
+    var props = {
+        votes: {
             good: 15,
             bad: 10
         },
         date: {
-            day: 8,
-            month: 6
+            day: 3,
+            month: 5
         },
         name: 'Kayne West',
         desc: 'lorem blablablabla',
@@ -19,50 +19,76 @@ export const Card = () => {
         img: 'assets/img/pope-francis.png'
     }
 
-    let voteText = 'Vote Now'
+    const voteState = {
+        vote: '',
+        card: `${props.name}`,
+        voteBtnText: 'Vote Now'
+    }
 
-    const [votes, setVotes] = useState(props.vote)
+    const [state, setState] = useState(voteState)
 
-    const clickVote = () => {
+    const voteChange = (e) => {
+        const vote = e.currentTarget.value;
+        setState((prev) => ({ ...prev, vote }));
+    };
+
+    const voteSubmit = (e) => {
+        e.preventDefault();
+    
+        if (!state.vote) {
+          setState({voteBtnText: "Select an option and vote!",});
+          return;
+        }
+        console.log('before', props.votes)
+    
+        if (state.vote == 'good') props.votes.good += 1
+            
+        else props.votes.bad += 1
+
+        setState({voteBtnText: "Vote Again",});
+        console.log('after', props.votes)
         
-    }
-
-    const clickOption = () => {
-
-    }
+    };
 
     const calcDate = (dateCard) => {
         let newDate = new Date()
         let day = newDate.getDate()
-        let month = newDate.getMonth()
+        let month = newDate.getMonth()+1
 
-        if(dateCard.month === month){
-            if (day == dateCard.day) return 'Posted today'
+        if(month == dateCard.month){
+            if (day == dateCard.day) return 'today'
+            else if (day == dateCard.day+1) return 'yesterday'
             else return day-dateCard.day + ' days ago'
         }
+        else if (month == dateCard.month+1) return 'last month'
         else return month-dateCard.month + ' months ago'
     }
 
     return (
         <div className="votecard__container">
             <img className="votecard__background-img" src={props.img} alt={props.name} title={props.name}/>
+            
             <div className="votecard__text">
                 <h2>{props.name}</h2>
                 <p>{props.desc}</p>
             </div>
+            
             <div className="votecard__vote-options">
-                <span className="votecard__vote-how-old">{calcDate(props.date)} in {props.category}</span>
-                <form className="votecard__form" action="#"> 
-                    <label className="votecard__form-option">
-                        <input type="image" src="assets/img/thumbs-up.svg"/>
-                    </label>
-                    <label className="votecard__form-option">
-                        <input type="image" src="assets/img/thumbs-down.svg"/>
-                    </label>
-                    <button className="votecard_vote-button" onClick={clickVote}>{voteText}</button>
+                <span className="votecard__vote-eyebrown-text">{calcDate(props.date)} in {props.category}</span>
+                
+                <form className="votecard__form" onSubmit={voteSubmit}> 
+                    <input type="radio" id="thumbs-up" name="vote" value="good" onChange={voteChange}/>
+                    <label className="votecard__form-input good" for="thumbs-up"><img src="assets/img/thumbs-up.svg" /></label>
+                    
+                    <input type="radio" id="thumbs-down" name="vote" value="bad" onChange={voteChange}/>
+                    <label className="votecard__form-input bad" for="thumbs-down"><img src="assets/img/thumbs-down.svg" /></label>
+                    
+                    <button className="votecard_vote-button" type="submit">{state.voteBtnText}</button>
                 </form>
+
             </div>
-            <VoteBar percentage={votes}/>
+
+            <VoteBar good={props.votes.good} bad={props.votes.bad}/>
         </div>
     )
 }
